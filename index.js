936d057bb713f63e9d5c902a68cec5f51a6cb059
Sony Chen <hello@sony-chen.com>
2024-03-23 09:17:15 -0600

    added js

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// document.querySelector(".profile-title-name").innerHTML="Sony, ";
-// document.querySelector(".profile-title-job").innerHTML="Account";
-
-// document.querySelector(".profile-img").src = "./assets/images/green.png";
-// document.querySelector(".profile-img-2").src = "./assets/images/green.png";
-
 const username = 'Sonni Chen,';
 const job = 'Sr product designer';
 const profileImage = "./assets/images/green.png";
